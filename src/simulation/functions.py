6c1b4ff9901122ea19a7ea6ef6ef726c5de6bf0c
dcuoliveira <dan.cunhaa@gmail.com>
2024-08-04 11:25:54 -0300

    add exp kernel

--- src/simulation/functions.py
+++ src/simulation/functions.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 def powerLawKernel(x, alpha = 1., t0 = 1., beta = -2.):
     if x < t0: return 0
@@ -12,3 +12,6 @@ def powerLawCutoff(time, alpha, beta, gamma):
 
 def powerLawKernelIntegral(x1, x2, alpha = 1., t0 = 1., beta = -2.):
     return (x2/(1+beta))*powerLawKernel(x2, alpha = alpha, t0=t0, beta=beta) - (x1/(1+beta))*powerLawKernel(x1, alpha = alpha, t0=t0, beta=beta)
+
+def expKernel(x, alpha, beta):
+    return alpha*np.exp(-x*beta)
